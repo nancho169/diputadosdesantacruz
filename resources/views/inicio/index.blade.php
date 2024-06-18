8e4747cd279a59cbc9683c244b60de164c8ac915
N4nch0 <nancho169@gmail.com>
2024-06-18 00:08:12 -0300

    pequeños cambios sigue 1.1v

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -74,7 +74,7 @@
 
     <!-- Three columns of text below the carousel -->
     <div class="row" >
-        <div class="col-lg-4" >
+        <div class="col-lg-3" >
             <div class="card shadow-lg p-3 mb-5 bg-body-tertiary rounded" style="width: 18rem; background-color: rgba(0, 0, 0, .1); border: solid rgba(0, 0, 0, .15);border-width: 1px 0;">
                 <!--<svg class="bd-placeholder-img rounded-circle" width="140" height="140"
                     xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder"
@@ -84,7 +84,7 @@
                 </svg>-->
                 <img src="{{ asset('img/logo.png') }}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h2 class="fw-normal">Heading</h2>
+                    <h2 class="fw-normal">Diputados</h2>
                     <p>Some representative placeholder content for the three columns of text below the carousel.
                         This is the first column.</p>
                     <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
@@ -92,7 +92,7 @@
             </div>
 
         </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4" >
+        <div class="col-lg-3" >
           <div class="card shadow-lg p-3 mb-5 bg-body-tertiary rounded" style="width: 18rem; background-color: rgba(0, 0, 0, .1); border: solid rgba(0, 0, 0, .15);border-width: 1px 0;">
               <!--<svg class="bd-placeholder-img rounded-circle" width="140" height="140"
                   xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder"
@@ -102,7 +102,7 @@
               </svg>-->
               <img src="{{ asset('img/logo.png') }}" class="card-img-top" alt="...">
               <div class="card-body">
-                  <h2 class="fw-normal">Heading</h2>
+                  <h2 class="fw-normal">Proyectos</h2>
                   <p>Some representative placeholder content for the three columns of text below the carousel.
                       This is the first column.</p>
                   <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
@@ -110,7 +110,7 @@
           </div>
 
       </div><!-- /.col-lg-4 -->
-      <div class="col-lg-4" >
+      <div class="col-lg-3" >
         <div class="card shadow-lg p-3 mb-5 bg-body-tertiary rounded" style="width: 18rem; background-color: rgba(0, 0, 0, .1); border: solid rgba(0, 0, 0, .15);border-width: 1px 0;">
             <!--<svg class="bd-placeholder-img rounded-circle" width="140" height="140"
                 xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder"
@@ -120,7 +120,25 @@
             </svg>-->
             <img src="{{ asset('img/logo.png') }}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h2 class="fw-normal">Heading</h2>
+                <h2 class="fw-normal">Cámara</h2>
+                <p>Some representative placeholder content for the three columns of text below the carousel.
+                    This is the first column.</p>
+                <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+            </div>
+        </div>
+
+    </div><!-- /.col-lg-4 -->
+    <div class="col-lg-3" >
+        <div class="card shadow-lg p-3 mb-5 bg-body-tertiary rounded" style="width: 18rem; background-color: rgba(0, 0, 0, .1); border: solid rgba(0, 0, 0, .15);border-width: 1px 0;">
+            <!--<svg class="bd-placeholder-img rounded-circle" width="140" height="140"
+                xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder"
+                preserveAspectRatio="xMidYMid slice" focusable="false">
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
+            </svg>-->
+            <img src="{{ asset('img/logo.png') }}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h2 class="fw-normal">Sesiones</h2>
                 <p>Some representative placeholder content for the three columns of text below the carousel.
                     This is the first column.</p>
                 <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
