69005c127402dd4e8d2beae1bc89a0612db7c0f6
nancho <hernan gimenez@info.hcd>
2024-06-19 14:22:01 -0300

    agregando toast para cuenta regresiva

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -73,6 +73,32 @@
 <!-- fin carousel -->
 
 
+<div class="toast fade show  position-fixed bottom-0 start-0 p-3" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header">
+        <svg class="bd-placeholder-img rounded me-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+            <rect width="100%" height="100%" fill="#FF0000"></rect>
+        </svg>
+        <strong class="me-auto">
+            <font style="vertical-align: inherit;">
+                <font style="vertical-align: inherit;">Proxima sesión</font>
+            </font>
+        </strong>
+        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Cerca"></button>
+    </div>
+    <div class="toast-body">
+        <section>
+            <span id="days"></span> días  - <span id="hours"></span> horas - <span id="minutes"></span> minutos - <span id="seconds"></span> segundos
+        </section>
+        <!--<font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">
+            <section>
+                    <span id="days"></span> días  - <span id="hours"></span> horas <br> <span id="minutes"></span> minutos - <span id="seconds"></span> segundos
+            </section>
+            </font>
+        </font>-->
+    </div>
+  </div>
+
 <div class="alert alert-dark" role="alert" style="text-align: center; width: 90%; margin: 0 auto;" data-aos="fade-up">
     <h3 >
         Descubre la Honorable Cámara de Diputados de Río Gallegos
@@ -82,6 +108,8 @@
         Bienvenido a la Honorable Cámara de Diputados de Río Gallegos, el órgano legislativo fundamental de la provincia de Santa Cruz, donde se forjan las leyes y se fortalece la democracia. Con una rica historia y un profundo compromiso con nuestra comunidad, la Cámara de Diputados no solo es un símbolo de la voluntad popular, sino también un espacio dinámico de debate y construcción de un futuro mejor para todos los santacruceños.
     </p>
     <button type="button" class="btn btn-light" id="speak-button"><i class="fa fa-volume-up" aria-hidden="true"></i>REPRODUCIR</button>
+    <hr>
+    
   </div>
 <br>
 <br>
@@ -246,7 +274,7 @@
 </div><!-- /.container -->
 
 
-
+<!-- Lectura -->
 <script>
     document.addEventListener('DOMContentLoaded', function () {
     // Verificar si el navegador soporta la síntesis de voz
@@ -274,6 +302,57 @@
         alert('La API de síntesis de voz no es soportada en este navegador.');
     }
 });
+
+//Contador
+
+
+//===
+// VARIABLES
+//===
+const DATE_TARGET = new Date('06/27/2024 0:01 PM');
+// DOM for render
+const SPAN_DAYS = document.querySelector('span#days');
+const SPAN_HOURS = document.querySelector('span#hours');
+const SPAN_MINUTES = document.querySelector('span#minutes');
+const SPAN_SECONDS = document.querySelector('span#seconds');
+// Milliseconds for the calculations
+const MILLISECONDS_OF_A_SECOND = 1000;
+const MILLISECONDS_OF_A_MINUTE = MILLISECONDS_OF_A_SECOND * 60;
+const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
+const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24
+
+//===
+// FUNCTIONS
+//===
+
+/**
+ * Method that updates the countdown and the sample
+ */
+function updateCountdown() {
+    // Calcs
+    const NOW = new Date()
+    const DURATION = DATE_TARGET - NOW;
+    const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY);
+    const REMAINING_HOURS = Math.floor((DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR);
+    const REMAINING_MINUTES = Math.floor((DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE);
+    const REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND);
+    // Thanks Pablo Monteserín (https://pablomonteserin.com/cuenta-regresiva/)
+
+    // Render
+    SPAN_DAYS.textContent = REMAINING_DAYS;
+    SPAN_HOURS.textContent = REMAINING_HOURS;
+    SPAN_MINUTES.textContent = REMAINING_MINUTES;
+    SPAN_SECONDS.textContent = REMAINING_SECONDS;
+}
+
+//===
+// INIT
+//===
+updateCountdown();
+// Refresh every second
+setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);
+
+
 
 
 
